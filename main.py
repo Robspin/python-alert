25997cfeccaee204b054f778b38c540bbc68e1b2
Robin Steeman <robin@tracefy.com>
2022-12-29 19:54:05 +0100

    list comprehension

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ import time
 
 url = ""
 
+
 def operational():
-    all_operational = False
+    category_statuses = []
     try:
         res = requests.get(url)
-        all_operational = res.status_code == 200
+        data = res.json()
+        for category in data:
+            new_list = [service['operational'] for service in  category['statuses']]
+            category_statuses = new_list
     except Exception as error:
         print(error)
     finally:
-        if all_operational:
+        print(category_statuses)
+        print(all(category_statuses))
+        if all(category_statuses):
             return True
         return False
+
 
 io.setmode(io.BCM)
 led_red = 27
@@ -24,18 +31,27 @@ io.setup(led_blue, io.OUT)
 
 result = operational()
 
+
+def green_light():
+    io.output(led_red, False)
+    io.output(led_blue, True)
+    time.sleep(1)
+
+
+def red_light():
+    io.output(led_blue, False)
+    io.output(led_red, True)
+    time.sleep(1)
+    io.output(led_red, False)
+    time.sleep(1)
+
+
 try:
     while True:
         if result:
-            io.output(led_red, False)
-            io.output(led_blue, True)
-            time.sleep(1)
+            green_light()
         else:
-            io.output(led_blue, False)
-            io.output(led_red, True)
-            time.sleep(1)
-            io.output(led_red, False)
-            time.sleep(1)
+            red_light()
 
 except KeyboardInterrupt:
     print("Keyboard interrupt")
