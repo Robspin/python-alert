a0bc50b88993431effe014ca755339f9ee675133
Robin Steeman <robin@tracefy.com>
2022-12-31 17:24:08 +0100

    hotfix

--- buzzer.py
+++ buzzer.py
@@ -6,10 +6,10 @@ from time import sleep
 io.setwarnings(False)
 # io.setmode(io.BCM)
 buzzer = 26
-io.setup(buzzer, io.OUT)
+# io.setup(buzzer, io.OUT)
 # tone1 = io.PWM(buzzer, 100)
 # tone1.start(50)
-io.setmode(io.BOARD)
+io.setmode(io.BCM)
 io.setup(buzzer, io.OUT)
 pin7 = io.PWM(buzzer, 100)
 pin7.start(50)
